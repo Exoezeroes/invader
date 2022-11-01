3d64cdf25538b6a26d40bfa638f9e6e6e688fe99
ZeroRaxo <marvellgiovano@gmail.com>
2022-11-01 15:21:42 +0700

    made basic main menu (not fully functioning yet)

---
+++ Invader/Assets/Scripts/mainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour
+{
+    public void Play()
+    {
+        Debug.Log("Play");
+        SceneManager.LoadScene("SampleScene");
+    }
+    public void Quit()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
